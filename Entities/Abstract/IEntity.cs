c2503e9720dc258e4c62d5b8f075a23d4aab6ce4
dilan yuşan <dyusaan@gmail.com>
2021-03-18 00:23:46 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{//IEntity implemet eden class bir veritabanaı tablosudur
+   public interface IEntity
+    {
+    }
+}
