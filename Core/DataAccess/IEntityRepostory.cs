93f4ca21fd59864bfde3058a60d603c6c8d8abf5
dilan yuşan <dyusaan@gmail.com>
2021-03-23 01:54:40 +0300

    23mart

--- Core/DataAccess/IEntityRepostory.cs
+++ Core/DataAccess/IEntityRepostory.cs
@@ -1,10 +1,11 @@
-﻿using Entities.Abstract;
+﻿//core katmanı diğer katmanları referans almaz
+using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     // generic constraint
     //class: referans tip
